bd80def364599550d799c543e0326f975527f38c
EvandroKaibara <yujievandro@gmail.com>
2025-06-02 21:59:14 -0300

    A

---
+++ src/routes/Home/Home.jsx
@@ -0,0 +1,33 @@
+
+import AbrigosProximos from './components/AbrigosProximos'
+import AlertasDeRisco from './components/AlertasDeRisco'
+import Banner from './components/Banner'
+import Footer from './components/Footer'
+import Header from './components/header'
+import InformacoesUteis from './components/InformacoesUteis'
+import Mudanca from './components/Mudancas'
+import SobreNos from './components/SobreNos'
+
+function App() {
+
+  return (
+    <>
+      <header>
+        <Header />
+      </header>
+      <main>
+        <Banner />
+        <AbrigosProximos />
+        <AlertasDeRisco />
+        <InformacoesUteis />
+        <Mudanca />
+        <SobreNos />
+      </main>
+      <footer>
+        <Footer />
+      </footer>
+    </>
+  )
+}
+
+export default App
